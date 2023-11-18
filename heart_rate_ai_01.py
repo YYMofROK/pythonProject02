5f4506819f4665c1867a1d4772932e705a4437c0
yuk-youngmin <a84146943@gmail.com>
2023-11-18 23:24:06 +0900

    좌우 미러 수정

--- heart_rate_ai_01.py
+++ heart_rate_ai_01.py
@@ -23,8 +23,11 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # Flip the frame horizontally
+    mirror_image = cv2.flip(frame, 1)
+
     # Convert to grayscale
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(mirror_image, cv2.COLOR_BGR2GRAY)
 
     # Detect faces using the cascade classifier
     faces = face_cascade.detectMultiScale(
@@ -48,16 +51,19 @@ while True:
         if len(heart_rates) >= 10:
             heart_rate_history = heart_rates[-10:]
             heart_rate_average = np.mean(heart_rate_history)
-            heart_rate_average = heart_rate_average * 0.7       # RGB 중에서 R 칼라 속성을 이용한 심박수 측정 가중치
+            heart_rate_average = heart_rate_average * 0.68       # RGB - R add value
+
+        # Draw a filled bounding box around the detected face
+        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), cv2.FILLED)
 
         # Display the current heart rate
-        cv2.putText(frame, f"Heart Rate: {heart_rate_average:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(mirror_image, f"Heart Rate: {heart_rate_average:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
         # Draw a bounding box around the detected face
-        draw_bounding_box(face_coordinates, frame, (0, 255, 0) )
+        draw_bounding_box(face_coordinates, mirror_image, (0, 255, 0) )
 
     # Display the captured frame
-    cv2.imshow(WINDOW_NAME, frame)
+    cv2.imshow(WINDOW_NAME, mirror_image)
 
     # Check for 'q' key to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
